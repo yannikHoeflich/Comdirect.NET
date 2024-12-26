88491666d62d49cf1f93172022fba7788e14b3b8
yannikHoeflich <ygidyyy@gmail.com>
2024-12-26 19:15:04 +0100

    Added empty test

---
+++ Comdirect.NET.Tests/ComdirectClientTest.cs
@@ -0,0 +1,14 @@
+﻿using Comdirect.NET;
+using NUnit.Framework;
+
+namespace Comdirect.NET.Tests;
+
+[TestFixture]
+[TestOf(typeof(ComdirectClient))]
+public class ComdirectClientTest {
+
+    [Test]
+    public void EmptyTest() {
+        
+    }
+}
